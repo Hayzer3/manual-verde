da56b535d53eb070a21e1ecfb93e81e722899482
Hayzer3 <lucashyzr@gmail.com>
2025-11-17 20:54:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        
+        burger.classList.toggle('toggle');
+    });
+}
+
+const closeMenuOnClick = () => {
+    const nav = document.querySelector('.nav-links');
+    const burger = document.querySelector('.burger');
+    const links = document.querySelectorAll('.nav-links li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            if (nav.classList.contains('nav-active')) {
+                nav.classList.remove('nav-active');
+                burger.classList.remove('toggle');
+            }
+        });
+    });
+}
+
+navSlide();
+closeMenuOnClick();
